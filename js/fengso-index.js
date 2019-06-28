233eabae5a90f3d4390cc7748492a34d3ef67ddb
windsailg <38423109+windsailg@users.noreply.github.com>
2019-06-28 10:14:31 +0800

    Add files via upload

--- js/fengso-index.js
+++ js/fengso-index.js
@@ -322,29 +322,29 @@ $('#IWantToTeach').LightingBox();
 });
 
 
-    var player;
+    // var player;
 
-    function onYouTubePlayerAPIReady() {
-        player = new YT.Player('player', {
-        //   height: '390',
-        //   width: '640',
-          videoId: 'MOVJYGeu93M',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-    }
+    // function onYouTubePlayerAPIReady() {
+    //     player = new YT.Player('player', {
+    //     //   height: '390',
+    //     //   width: '640',
+    //       videoId: 'MOVJYGeu93M',
+    //       events: {
+    //         'onReady': onPlayerReady,
+    //         'onStateChange': onPlayerStateChange
+    //       }
+    //     });
+    // }
 
-    // autoplay video
-    function onPlayerReady(event) {
-       event.target.playVideo();
-    }
+    // // autoplay video
+    // function onPlayerReady(event) {
+    //    event.target.playVideo();
+    // }
 
-    // when video ends
-    function onPlayerStateChange(event) {        
-        if(event.data === 0) {            
-            // alert('completed');
-        }
-    }
+    // // when video ends
+    // function onPlayerStateChange(event) {        
+    //     if(event.data === 0) {            
+    //         // alert('completed');
+    //     }
+    // }
  
