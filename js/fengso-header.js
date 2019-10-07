c54efcff599d9cd285c99e9398b354a8da0e8c61
tkb0003602 <tkb0003602@gmail.com>
2019-10-07 15:53:32 +0800

    更新header searcher 的RWD顯示問題

--- js/fengso-header.js
+++ js/fengso-header.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 
    //Header Controls
    $('#HeaderSearchRWDTrigger').click(function(){
+      
       $('#HeaderSearchBlock').fadeToggle(220);
    })
 
@@ -42,20 +43,6 @@ $(document).ready(function(){
       }
    })
 
-var resizeTimer = null;
-   $(window).resize(function(){
-         if (resizeTimer) {
-           clearTimeout(resizeTimer)
-         }
-         resizeTimer = setTimeout(function(){
-            var ww = window.innerWidth;
-            if(ww <= 781){
-               $('#HeaderSearchBlock').hide();
-            }else{
-               $('#HeaderSearchBlock').show();
-            }
-         }, 500);
-   })
 
 // $('#SideBarITeach').ightingBoxType2();
 // $('#HeaderITeach').LightingBoxType2();
